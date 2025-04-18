14f2a5bfe12c9210f1295988c8c8901ef30f12c8
alantothe <rubenamalpartida@gmail.com>
2025-04-18 01:09:24 -0400

    work in progress...

--- src/utils/auth-config.ts
+++ src/utils/auth-config.ts
@@ -1,14 +1,9 @@
 import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-//~-------------------------------------
-// Choose your database:
-// Local Postgres:
-// import db from "../db/client/client-pg-local";
 
-// Neon (for serverless):
-// import db from "../db/client/client-neon";
-//~-------------------------------------
+import db from "../db/client/client-neon";
+
 import { schema } from "../db/schema";
 
 export const auth = betterAuth({
